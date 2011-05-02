2108e2ca5de899bc13324cdbbda2ffe8f3d70850
T Chandirasekar <chandru9279@gmail.com>
2011-05-02 18:43:09 +0530

    Chandru : Made routing on host name work on IIS. Transfer Result not working in IIS

--- zasz.me/Controllers/IndirectionController.cs
+++ zasz.me/Controllers/IndirectionController.cs
@@ -1,22 +1,10 @@
 ﻿using System.Web.Mvc;
-using zasz.me.Controllers.Utils;
-using zasz.me.Models;
 
 namespace zasz.me.Controllers
 {
     /* Need to delete this controller and use Routing to do this*/
     public class IndirectionController : Controller
     {
-        public ActionResult Default()
-        {
-            return Indirect();
-        }
-
-        public ActionResult Indirect()
-        {
-            return new TransferResult(Site.WithHost(Request.Url.Host).VirtualPath);
-        }
-
         public ActionResult Favicon()
         {
             return new FilePathResult("~/Content/favicon.png", "image/png");
--- zasz.me/Global.asax.cs
+++ zasz.me/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using zasz.me.Integration;
@@ -22,13 +23,32 @@ namespace zasz.me
             Routes.IgnoreRoute("{CustomEndpoint}.axd");
             Routes.IgnoreRoute("Content/");
             Routes.MapRoute("Favicon", "favicon.ico", new {Controller = "Indirection", Action = "Favicon"});
-
+            
+            /*Routing based on Domain to Areas : 
+             * http://www.asp.net/mvc/tutorials/creating-a-custom-route-constraint-cs,
+             * http://suhair.in/Blog/Aspnet-MVC-Areas-in-depth-Part-2
+             */
 
             Routes.MapRoute(
-                "Indirection", 
+                "zasz.me Route",
                 "{Controller}/{Action}/{Id}",
-                new {Controller = "Indirection", Action = "Default", Id = UrlParameter.Optional} // Parameter defaults
-                );
+                new {Controller = "Home", Action = "Show", Id = UrlParameter.Optional},
+                new {DomainConstraint = new DomainRouteConstraint("zasz.me")} 
+                ).DataTokens["area"] = "Pro";
+
+            Routes.MapRoute(
+                "localhost Route",
+                "{Controller}/{Action}/{Id}",
+                new {Controller = "Home", Action = "Show", Id = UrlParameter.Optional},
+                new {DomainConstraint = new DomainRouteConstraint("localhost")}
+                ).DataTokens["area"] = "Pro";
+
+            Routes.MapRoute(
+                "chandruon.net Route",
+                "{Controller}/{Action}/{Id}",
+                new {Controller = "Home", Action = "Show", Id = UrlParameter.Optional},
+                new {DomainConstraint = new DomainRouteConstraint("chandruon.net")}
+                ).DataTokens["area"] = "Pro";
         }
 
         protected void Application_Start()
@@ -41,5 +61,29 @@ namespace zasz.me
             UnityIntegration.Bootstrap();
             EntityFramework.Bootstrap();
         }
+
+        #region Nested type: DomainRouteConstraint
+
+        public class DomainRouteConstraint : IRouteConstraint
+        {
+            private readonly string _Domain;
+
+            public DomainRouteConstraint(string Domain)
+            {
+                _Domain = Domain;
+            }
+
+            #region IRouteConstraint Members
+
+            public bool Match(HttpContextBase Context, Route Route, string ParameterName, RouteValueDictionary Values, RouteDirection RouteDirection)
+            {
+                var Url = Context.Request.Url.Host;
+                return Url.Equals(_Domain, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            #endregion
+        }
+
+        #endregion
     }
 }
