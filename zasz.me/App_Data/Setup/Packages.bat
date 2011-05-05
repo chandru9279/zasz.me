e74fdf61d7ccf3ef0313783259b8769fd60bcd26
T Chandirasekar <chandru9279@gmail.com>
2011-05-05 17:09:20 +0530

    Chandru: Added Agility pack to packages.bat

--- zasz.me/App_Data/Setup/Packages.bat
+++ zasz.me/App_Data/Setup/Packages.bat
@@ -6,4 +6,5 @@ if not exist elmah.1.1 nuget install elmah -version 1.1
 if not exist AntiXSS.4.0.1 nuget install AntiXSS -version 4.0.1
 if not exist Moq.4.0.10827 nuget install Moq -version 4.0.10827
 if not exist EntityFramework.4.1.10331.0 nuget install EntityFramework -version 4.1.10331.0
+if not exist HtmlAgilityPack.1.4.0 nuget install HtmlAgilityPack -version 1.4.0
 pause
