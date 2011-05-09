9992e5d33285aa5e6ca93c1d53cf203ad776b702
Chandru <chandru9279@gmail.com>
2011-05-10 00:51:54 +0530

    Chandru : SAD ARchive query problem

--- zasz.me/Integration/EntityFramework/Posts.cs
+++ zasz.me/Integration/EntityFramework/Posts.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using zasz.me.Areas.Shared.Models;
 
@@ -18,7 +19,7 @@ namespace zasz.me.Integration.EntityFramework
 
         public override void Save(Post Instance)
         {
-            List<Tag> PersistantTags = new List<Tag>(Instance.Tags.Count);
+            var PersistantTags = new List<Tag>(Instance.Tags.Count);
             PersistantTags.AddRange(from Tag in Instance.Tags
                                     select _Tags.FindOrNew(Tag.Name));
             Instance.Tags = PersistantTags;
@@ -34,7 +35,7 @@ namespace zasz.me.Integration.EntityFramework
 
         public List<Post> Page(int PageNumber, int PageSize)
         {
-            return _ModelSet.OrderBy(Model => Model.Timestamp).Skip(PageNumber * PageSize).Take(PageSize).ToList();
+            return _ModelSet.OrderBy(Model => Model.Timestamp).Skip(PageNumber*PageSize).Take(PageSize).ToList();
         }
 
         /// <param name="PageNumber">Zero-Based Index</param>
@@ -43,12 +44,50 @@ namespace zasz.me.Integration.EntityFramework
             return (from Model in _ModelSet
                     where Model.Site.Name == ProOrRest.Name || Model.Site.Name == "Both"
                     orderby Model.Timestamp
-                    select Model).Skip(PageNumber * PageSize).Take(PageSize).ToList();
+                    select Model).Skip(PageNumber*PageSize).Take(PageSize).ToList();
         }
 
         public int Count(Site ProOrRest)
         {
             return _ModelSet.Where(Model => Model.Site.Name == ProOrRest.Name || Model.Site.Name == "Both").Count();
+        }
+
+        #endregion
+
+        /// <returns> A Dictionary of year as Key and the list of formatted months on which posts have been published as value</returns>
+        public Dictionary<int, List<string>> PostedMonths(Site ProOrRest)
+        {
+            Dictionary<int, List<string>> Result = new Dictionary<int, List<string>>();
+            IQueryable<DateTime> Dates = (from Model in _ModelSet
+                                          where Model.Site.Name == ProOrRest.Name || Model.Site.Name == "Both"
+                                          select Model.Timestamp);
+            Dictionary<DateTime, string> Dict = Dates.ToDictionary(It => new DateTime(It.Year, It.Month, 1), It => string.Format("{0:MMMM, yyyy}", It));
+            var a = from Pair in Dict.Distinct(new FormattedDateComparator())
+                    group Pair by Pair.Key.Year into Group
+                    select new {Group.Key, Group};
+            foreach (var Pair in Dict.Distinct(new FormattedDateComparator()).OrderBy(It => It.Key).GroupBy(It => It.Key.Year))
+            {
+                Result.Add(Pair.Key, Pair.ToList());
+            }
+        }
+
+        #region Nested type: FormattedDateComparator
+
+        private class FormattedDateComparator : IEqualityComparer<KeyValuePair<DateTime, string>>
+        {
+            #region IEqualityComparer<KeyValuePair<DateTime,string>> Members
+
+            public bool Equals(KeyValuePair<DateTime, string> X, KeyValuePair<DateTime, string> Y)
+            {
+                return X.Value == Y.Value;
+            }
+
+            public int GetHashCode(KeyValuePair<DateTime, string> Obj)
+            {
+                return Obj.GetHashCode();
+            }
+
+            #endregion
         }
 
         #endregion
