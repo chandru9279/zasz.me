ee8116b2f23ec36112bc726cc81b684c874b12aa
T Chandirasekar <chandru9279@gmail.com>
2011-10-06 23:50:52 +0530

    Chandru: setting Twlaptop to defalut for sometime, and added clean.bat

---
+++ Data-Tools-Setup/Clean.bat
@@ -0,0 +1,16 @@
+cd ..\zasz.develop
+rd /S /Q bin
+rd /S /Q obj
+md bin
+md obj
+cd ..\zasz.health
+rd /S /Q bin
+rd /S /Q obj
+md bin
+md obj
+cd ..\zasz.me
+rd /S /Q bin
+rd /S /Q obj
+md bin
+md obj
+pause
