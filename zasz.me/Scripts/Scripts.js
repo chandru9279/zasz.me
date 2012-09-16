1db4fdecfa180d77267b45765caf64701284c9d6
T Chandirasekar <chandru9279@gmail.com>
2012-09-17 00:55:03 +0530

    dotless, coffeesript, better icons, white theme. fixed many css problems

---
+++ zasz.me/Scripts/Scripts.js
@@ -0,0 +1,99 @@
+(function() {
+  var addJqueryCenter, addJqueryModal, attachHandlers, closeStatus, expandStatus;
+
+  attachHandlers = function() {
+    $("body").click(closeStatus);
+    $("#Status-History-Expand-Button").click(expandStatus);
+    $(".Expand-Button").click(function() {
+      return $(this).next().slideToggle();
+    });
+    $(".Modal").modalpop({
+      speed: 300
+    });
+    $("#Search-Box").keypress(function(e) {
+      if (e.which === 13) {
+        $("#SearchForm").submit();
+        return true;
+      }
+      return true;
+    });
+    $("#Search-Box").autocomplete({
+      source: "/Home/Autocomplete"
+    });
+    if ($("#blogTagCloud").length > 0) {
+      return $("#blogTagCloud").load(function() {
+        return $.get("/Blog/TagCloudLinks", function(links) {
+          return $("#tagCloudLinks").replaceWith(links);
+        });
+      });
+    }
+  };
+
+  expandStatus = function() {
+    if (statusExpanded) {
+      closeStatus();
+    } else {
+      $("#Status-History").toggleClass("Status-History Status-History-Expanded");
+      window.statusExpanded = true;
+    }
+    return false;
+  };
+
+  closeStatus = function() {
+    if (statusExpanded) {
+      $("#Status-History").removeClass("Status-History-Expanded");
+      $("#Status-History").addClass("Status-History");
+      return window.statusExpanded = false;
+    }
+  };
+
+  addJqueryCenter = function() {
+    return jQuery.fn.center = function() {
+      this.css("position", "absolute");
+      this.css("top", ($(window).height() - this.outerHeight()) / 2 + $(window).scrollTop() + "px");
+      this.css("left", ($(window).width() - this.outerWidth()) / 2 + $(window).scrollLeft() + "px");
+      return this;
+    };
+  };
+
+  addJqueryModal = function() {
+    return jQuery.fn.modalpop = function(options) {
+      var defaults, height, width;
+      defaults = {
+        speed: 500,
+        center: false
+      };
+      $.extend(defaults, options);
+      width = $(window).width();
+      height = $(document).height();
+      jQuery("body").prepend("<div id='mask'></div>");
+      jQuery("#mask").css("height", height);
+      jQuery("#mask").css("width", width);
+      return this.each(function() {
+        jQuery(this).click(function() {
+          var $this, id;
+          $this = jQuery(this);
+          id = $this.attr("href");
+          $(id).center();
+          $("#mask").css("filter", "alpha(opacity=80)");
+          jQuery("#mask").fadeIn(defaults.speed);
+          jQuery(id).fadeIn(defaults.speed);
+          return false;
+        });
+        return jQuery("#mask").click(function() {
+          jQuery(this).fadeOut(defaults.speed);
+          return jQuery(".Pro-Popup").fadeOut(defaults.speed);
+        });
+      });
+    };
+  };
+
+  $(document).ready(function() {
+    addJqueryCenter();
+    addJqueryModal();
+    return attachHandlers();
+  });
+
+  window.statusExpanded = false;
+
+}).call(this);
