bf7024334ac599cade9394f9057a55046660e68b
Zasz <chandru9279@gmail.com>
2012-10-27 15:46:04 +0530

    using google cdn and iis compression

---
+++ zasz.me/ViewModels/FallbackModel.cs
@@ -0,0 +1,16 @@
+﻿namespace zasz.me.ViewModels
+{
+    public class FallbackModel
+    {
+        public FallbackModel(string lib, string condition, string local)
+        {
+            Lib = lib;
+            Condition = condition;
+            Local = local;
+        }
+
+        public string Lib { get; set; }
+        public string Condition { get; set; }
+        public string Local { get; set; }
+    }
+}
