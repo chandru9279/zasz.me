bcb5301288da86d36117371b1bbcae0a0906663a
T Chandirasekar <T Chandirasekar>
2014-08-31 20:12:11 +0530

    upgrade mvc, using mvc as nuget package, upgrade razor, remove RG.mvc, remove Webapplication targets

--- Build/default.ps1
+++ Build/default.ps1
@@ -14,7 +14,6 @@ task default -depends opendev
 
 
 task build -depends clean {
-	#Weird .net issue Any CPU vs AnyCPU
 	exec { msbuild $SolutionPath\zasz.me\zasz.me.csproj /p:Configuration=Server /p:Platform=AnyCPU /p:OutputPath=$BuildPath /nologo /verbosity:minimal}
 	Write-Host -ForegroundColor Green "Server build complete."
 }
